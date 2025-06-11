34ac653e19bbfd2f0a1268450faf4f2344bb5288
xulijing <xulijing@zhangyue.com>
2025-06-11 17:24:43 +0800

    feat: 初始化

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author: xulijing xulijing@zhangyue.com
+ * @Date: 2025-04-29 11:49:23
+ * @LastEditors: xulijing xulijing@zhangyue.com
+ * @LastEditTime: 2025-06-11 17:05:11
+ * @Description: file content
+ * @FilePath: /my-vue-app/src/App.vue
+-->
+
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Helvetica Neue', Helvetica,
+    Segoe UI, Arial, Roboto, 'PingFang SC', 'miui', 'Hiragino Sans GB', 'Microsoft Yahei',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: xulijing xulijing@zhangyue.com
+ * @Date: 2025-04-29 11:49:23
+ * @LastEditors: xulijing xulijing@zhangyue.com
+ * @LastEditTime: 2025-06-11 17:05:34
+ * @Description: file content
+ * @FilePath: /my-vue-app/src/main.ts
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+
+
+
+app.mount('#app')
